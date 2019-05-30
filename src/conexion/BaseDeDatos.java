e1a4e47910762e214f4ad52a3b54db3c06da63ae
freddy <freddy@GhostX>
2019-05-30 17:59:23 -0600

    resetup mysql port

--- src/conexion/BaseDeDatos.java
+++ src/conexion/BaseDeDatos.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  * @author Alicer
  */
 public class BaseDeDatos {
-    private final String URL = "jdbc:mysql://tcp.ngrok.io:16506/"; // Ubicación de la BD.
+    private final String URL = "jdbc:mysql://tcp.ngrok.io:17152/"; // Ubicación de la BD.
     private final String BD = "cafebar"; // Nombre de la BD.
     private final String USER = "freddy";
     private final String PASSWORD = "freddy";
