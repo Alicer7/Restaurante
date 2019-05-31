94d285149ed77ac86dc18a72add43c8071187d4e
freddy <freddy@GhostX>
2019-05-30 20:02:16 -0600

    add main class

---
+++ src/core/main.java
@@ -0,0 +1,23 @@
+package core;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import gui.MenuPrincipal;
+
+/**
+ *
+ * @author freddy
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        MenuPrincipal gui = new MenuPrincipal();
+        gui.pack();
+        
+        gui.setVisible(true);
+    }
+}
